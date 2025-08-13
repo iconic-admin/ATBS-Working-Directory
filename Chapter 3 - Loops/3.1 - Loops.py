4a584fba1e0b40458ae4d1ef9cb8ae5eb8a5d79c
Patrick Carr <pcarr1776@gmail.com>
2025-08-12 19:25:29 -0500

    chapter 3 complete

---
+++ Chapter 3 - Loops/3.1 - Loops.py
@@ -0,0 +1,13 @@
+# 1. ctrl+c
+# 2. break stops a loop, continue goes back to the top of the loop
+# 3. loops from 0 to 10, loops from 0 to 10, loops from 0 to 10 in steps of 1
+# 4
+
+for i in range (1, 11, 1):
+    print(i)
+
+i = 1
+
+while i < 11:
+    print(i)
+    i += 1
