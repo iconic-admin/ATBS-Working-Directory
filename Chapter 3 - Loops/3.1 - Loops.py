c6d496d8d929a0de76305c853490cc330032de09
Patrick Carr <pcarr1776@gmail.com>
2025-08-12 19:26:16 -0500

    chapter 3 complete

--- Chapter 3 - Loops/3.1 - Loops.py
+++ Chapter 3 - Loops/3.1 - Loops.py
@@ -1,7 +1,7 @@
 # 1. ctrl+c
 # 2. break stops a loop, continue goes back to the top of the loop
 # 3. loops from 0 to 10, loops from 0 to 10, loops from 0 to 10 in steps of 1
-# 4
+# 4.
 
 for i in range (1, 11, 1):
     print(i)
@@ -11,3 +11,5 @@ i = 1
 while i < 11:
     print(i)
     i += 1
+
+# 5. spam.bacon()
